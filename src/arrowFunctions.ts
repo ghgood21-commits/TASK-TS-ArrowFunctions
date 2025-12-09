6c1669bb2a7287a1b2f50391ae603dcfd456a2f4
Ghanim <your.ghgood21@yahoo.com>
2025-12-09 10:53:34 +0300

    Complete arrow functions task

--- src/arrowFunctions.ts
+++ src/arrowFunctions.ts
@@ -5,9 +5,15 @@
  */
 
 // Comment out this function before starting your solution
-function greet(name: string): string {
-  return `Hello ${name}`;
+// function greet(name: string): string {
+  // return `Hello ${name}`;
+// }
+let greet =(name:string) => {
+return `Hello ${name}`
+
+
 }
+
 
 // example:
 greet("Aziz"); // => "Hello Aziz"
@@ -24,8 +30,11 @@ greet("Aziz"); // => "Hello Aziz"
  */
 
 // Comment out this function before starting your solution
-function sum(a: number, b: number): number {
-  return Infinity;
+// function sum(a: number, b: number): number {
+  // return Infinity;
+// }
+let sum = (a:number, b:number) => {
+  return a + b
 }
 
 // example:
@@ -41,9 +50,14 @@ sum(5, 7); // => 12
  */
 
 // Comment out this function before starting your solution
-function square(n: number): number {
-  return Infinity;
+// function square(n: number): number {
+  // return Infinity;
+// }
+let square = (n:number) => {
+  return n * n
+
 }
+  
 
 // example:
 square(4); // => 16
